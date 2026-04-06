053925a3044363da2b864d592e50c0f927c35e65
Tharieel <thari.business@protonmail.com>
2026-04-06 19:48:40 +0200

    changes - startseite

---
+++ script.js
@@ -0,0 +1,14 @@
+let favNo = 16
+console.log("My favourite number is " + favNo)
+
+let age = 20
+if (age < 18) {
+    console.log("You are a minor.")
+} else {
+    console.log("You are an adult.")
+}
+
+let num1 = 10
+let num2 = 5
+let sum = num1 + num2
+console.log("The sum of " + num1 + " and " + num2 + " is " + sum)
